82a529536d131b4d398f89d9947ec566e0cfc6aa
Mitja Felicijan <mitja.felicijan@gmail.com>
2024-11-04 21:26:15 +0100

    Added categories to select unique stories

--- voicebox/select_unique_stories.py
+++ voicebox/select_unique_stories.py
@@ -6,6 +6,7 @@ import openai
 import dotenv
 import logging
 
+from feeds import feeds
 from pydantic import BaseModel
 from openai import OpenAI
 
@@ -24,14 +25,7 @@ conn = sqlite3.connect("cache.db")
 conn.row_factory = sqlite3.Row
 cur = conn.cursor()
 
-# Get stories from cache database.
-user_prompt = []
-cur.execute("SELECT * FROM stories WHERE cdate=?", (today,))
-rows = cur.fetchall()
-for row in rows:
-    user_prompt.append(", ".join((row["hash"], row["title"])))
-
-# Make call to OpenAI to get unique stories out.
+# OpenAI structured response model.
 class Story(BaseModel):
     title: str
     hash: str
@@ -39,29 +33,43 @@ class Story(BaseModel):
 class Stories(BaseModel):
     stories: list[Story]
 
+# System prompt for OpenAI.
 sys_prompt = []
 sys_prompt.append("You are a content editor working for an online publication and you select only newsworthy and contextually unique stories.")
 sys_prompt.append("Each line consists of story ID as a hash and a title.")
 sys_prompt.append("Return back the list of just 20 unique stories.")
 
-completion = oai_client.beta.chat.completions.parse(
-    model="gpt-4o-mini",
-    response_format=Stories,
-    messages=[
-        {"role": "system", "content": "\n".join(sys_prompt)},
-        {"role": "user", "content": "\n".join(user_prompt)}
-    ],
-)
+# Loop through all categories.
+cur.execute("SELECT DISTINCT(category) AS category FROM stories WHERE cdate=?", (today,))
+categories = cur.fetchall()
+for category in categories:
+    category = category["category"]
 
-# Update database by setting selected stories flag to 1.
-stories = completion.choices[0].message.parsed.stories
-for story in stories:
-    try:
-        logging.info(f"Selected: {story.title}")
-        cur.execute("UPDATE stories SET selected=1 WHERE hash=?", (story.hash,))
-        conn.commit()
-    except:
-        logging.critical("Error selecting: {story.title}")
+    # Get stories from cache database.
+    user_prompt = []
+    cur.execute("SELECT * FROM stories WHERE cdate=? AND category=?", (today, category))
+    stories = cur.fetchall()
+    for story in stories:
+        user_prompt.append(", ".join((story["hash"], story["title"])))
+
+    completion = oai_client.beta.chat.completions.parse(
+        model="gpt-4o-mini",
+        response_format=Stories,
+        messages=[
+            {"role": "system", "content": "\n".join(sys_prompt)},
+            {"role": "user", "content": "\n".join(user_prompt)}
+        ],
+    )
+
+    # Update database by setting selected stories flag to 1.
+    selected_stories = completion.choices[0].message.parsed.stories
+    for story in selected_stories:
+        try:
+            logging.info(f"Selected: {category} - {story.title}")
+            cur.execute("UPDATE stories SET selected=1 WHERE hash=?", (story.hash,))
+            conn.commit()
+        except:
+            logging.critical("Error selecting: {story.title}")
 
 conn.close()
 
